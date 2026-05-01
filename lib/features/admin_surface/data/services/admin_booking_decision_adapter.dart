9e28f8bf05909dd5fa6e529de5bdf179049af460
Bassem <kingbasbosa@gmail.com>
2026-05-01 12:16:56 +0300

    refactor(admin): isolate booking decision writes behind adapter

--- lib/features/admin_surface/data/services/admin_booking_decision_adapter.dart
+++ lib/features/admin_surface/data/services/admin_booking_decision_adapter.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 
 typedef AdminBookingRefreshAuthCallback = Future<void> Function({
   required String stage,
