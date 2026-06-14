9190410077551804022f7d0724a7945f573eea61
Bassem <kingbasbosa@gmail.com>
2026-06-15 00:38:19 +0300

    Remove Mental Key localization residue from active app

--- lib/l10n/app_localizations_en.dart
+++ lib/l10n/app_localizations_en.dart
@@ -9,7 +9,7 @@ class AppLocalizationsEn extends AppLocalizations {
   AppLocalizationsEn([String locale = 'en']) : super(locale);
 
   @override
-  String get appTitle => 'Mental Key';
+  String get appTitle => 'Mental Smile';
 
   @override
   String get homeTitle => 'Home';
